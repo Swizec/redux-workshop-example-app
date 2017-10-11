0e2459acc2b599da5d92960ea9445efad0e48d90
Swizec Teller <swizec@swizec.com>
2017-10-11 03:34:14 -0700

    Routing between cart/checkout

--- src/ShoppingCart.js
+++ src/ShoppingCart.js
@@ -1,10 +1,12 @@
 import React from "react";
 import { connect } from "react-redux";
 import styled from "styled-components";
+import { Route, withRouter } from "react-router";
+import { Link } from "react-router-dom";
 
 import { EventList } from "./Events";
 import { EventStyle, Event } from "./Event";
-import { Button } from "./FormElements";
+import { Button, ManagedInput } from "./FormElements";
 import { removeFromCart } from "./actions";
 
 class RemovableEvent extends React.Component {
@@ -35,23 +37,53 @@ const RemovableEventContainer = connect(null, {
     removeFromCart
 })(RemovableEvent);
 
+const Home = ({ items, match }) => (
+    <div>
+        <h1>Your shopping cart</h1>
+        <EventList events={items}>
+            {({ event }) => (
+                <RemovableEventContainer event={event} key={event.id} />
+            )}
+        </EventList>
+        {items.length > 0 ? (
+            <Link to={`${match.url}/checkout`}>Checkout</Link>
+        ) : null}
+    </div>
+);
+
+const Checkout = ({ items, match }) => (
+    <div>
+        <h1>Checkout {items.length} tickets</h1>
+        <Link to="/cart">Back</Link>
+    </div>
+);
+
 class ShoppingCart extends React.Component {
     render() {
-        const { items } = this.props;
+        const { items, match } = this.props;
 
         return (
             <div>
-                <h1>Your shopping cart</h1>
-                <EventList events={items}>
-                    {({ event }) => (
-                        <RemovableEventContainer event={event} key={event.id} />
+                <Route
+                    exact
+                    path={`${match.url}/`}
+                    component={({ match }) => (
+                        <Home items={items} match={match} />
                     )}
-                </EventList>
+                />
+                <Route
+                    path={`${match.url}/checkout`}
+                    component={({ match }) => (
+                        <Checkout items={items} match={match} />
+                    )}
+                />
             </div>
         );
     }
 }
 
-export default connect(state => ({
-    items: state.shoppingCart.items
-}))(ShoppingCart);
+export default withRouter(
+    connect(state => ({
+        items: state.shoppingCart.items
+    }))(ShoppingCart)
+);
